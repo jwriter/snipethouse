3c271217ecc3539c4f5bf47376eff1d924b1d5a7
Maxim <makbykov@yandex.ru>
2017-09-06 22:12:40 +0300

    добавил заглушку модели

---
+++ SnippetWarehouse/SnippetWebEditor/Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnippetWebEditor.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        //public Item Parent { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Item ItemId { get; set; }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public Item ItemId { get; set; }
+
+        public string Title { get; set; }
+    }
+}
