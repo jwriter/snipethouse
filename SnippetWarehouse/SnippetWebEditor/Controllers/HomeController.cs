54edcae724a9816837b5a028f034f9cd155dd21b
Max <upornij@gmail.com>
2017-09-07 17:53:17 +0300

    create ViewModel

--- SnippetWarehouse/SnippetWebEditor/Controllers/HomeController.cs
+++ SnippetWarehouse/SnippetWebEditor/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SnippetWebEditor.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,49 @@ namespace SnippetWebEditor.Controllers
 {
     public class HomeController : Controller
     {
+
+        ViewModel vm;
+        public HomeController()
+        {
+            
+        }
+
+
+        private ViewModel MakeVM(List<Note> notes, List<Item> items, int itemId = 0, int noteId = 0)
+        {
+            ViewModel res = new ViewModel();
+            if (itemId > 0)
+            {
+                Item parent = null;
+                Item current = items.Where(i => i.Id == itemId).FirstOrDefault();
+                if (current == null)
+                {
+                    //TODO
+                    throw new Exception();
+                }
+                Item tmp = current;
+                do
+                {
+                    parent = tmp.ItemId;
+                    List<Item> childs = items.Where(i => i.ItemId == parent).ToList();
+                    if (childs.Count > 0)
+                    {
+                        ChossedList cl = new ChossedList(childs, childs.IndexOf(tmp));
+                    }
+                    tmp = parent;
+                } while (parent != null);
+            }
+            else
+            {
+
+            }
+
+
+            return res;
+        }
+
+        
+
         public ActionResult Index()
         {
             return View();
@@ -27,4 +71,26 @@ namespace SnippetWebEditor.Controllers
             return View();
         }
     }
+
+    public class ViewModel
+    {
+        public List<ChossedList> table { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+    }
+
+    public class ChossedList
+    {
+        public List<int> Parts { get; set; }
+        public int ChoosedPart { get; set; }
+
+        public ChossedList(List<int> list, int place) 
+        {
+            Parts = list;
+            ChoosedPart = place;
+        }
+    }
+
+
 }
