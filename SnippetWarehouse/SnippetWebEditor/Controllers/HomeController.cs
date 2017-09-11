0be1fae7d8d2287f71a9adc2316bbb1a4f8179a7
Maxim <makbykov@yandex.ru>
2017-09-11 08:54:48 +0300

    ViewModel

--- SnippetWarehouse/SnippetWebEditor/Controllers/HomeController.cs
+++ SnippetWarehouse/SnippetWebEditor/Controllers/HomeController.cs
@@ -14,6 +14,13 @@ namespace SnippetWebEditor.Controllers
             return View();
         }
 
+        public ActionResult Table()
+        {
+            Repository repo = new Repository();
+            ViewModel vm = new ViewModel();
+            return View(vm.MakeVM(repo.GetNotes(), repo.GetItems()));
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -32,58 +39,104 @@ namespace SnippetWebEditor.Controllers
     public class ViewModel
     {
         public List<ChossedList> Table { get; set; } = new List<ChossedList>();
+        public List<Note> Notes { get; set; } = new List<Note>();
         public string Title { get; set; }
         public string Text { get; set; }
 
-
-        public ViewModel MakeVM(List<Note> notes, List<Item> items, int itemId = 1, int noteId = 1)
+        public ViewModel MakeVM(List<Note> notes, List<Item> items)
         {
-            ViewModel res = new ViewModel();
-            if (itemId > 0)
+            var choose = items.Where(i => i.ItemId == null).First();
+            return MakeVmForItem(notes, items, choose);
+        }
+
+        public ViewModel MakeVM(List<Note> notes, List<Item> items, int itemId, int noteId)
+        {
+
+
+            return null;
+        }
+
+        public ViewModel MakeVmItemId(List<Note> notes, List<Item> items, int itemId)
+        {
+            var currents = items.Where(i => i.Id == itemId).ToList();
+            Item choose;
+            if (currents.Count == 1)
+                choose = currents[0];
+            else if (currents.Count == 0)
             {
-                Item parent = null;
-                Item current = items.Where(i => i.Id == itemId).FirstOrDefault();
-                if (current == null)
+                choose = items.Where(i => i.ItemId == null).First();
+            }
+            else
+                choose = items.Where(i => i.ItemId == null).First();
+            return MakeVmForItem(notes, items, choose);
+        }
+
+        public ViewModel MakeVmForItem(List<Note> notes, List<Item> items, Item current)
+        {
+            if (!items.Contains(current))
+                throw new  ArgumentException();
+
+            ViewModel res = new ViewModel();            
+            
+            Item parent = null;
+            Item tmp = current;
+            //get parents
+            do
+            {
+                parent = tmp.ItemId;
+                List<Item> childs = items.Where(i => i.ItemId == parent).ToList();
+                if (childs.Count > 0)
                 {
-                    //TODO
-                    throw new Exception();
+                    ChossedList cl = new ChossedList(childs, childs.IndexOf(tmp));
+                    res.Table.Add(cl);
                 }
-                Item tmp = current;
-                do
+                tmp = parent;
+            } while (parent != null);
+            res.Table.Reverse();
+            parent = current;
+            List<Item> nodes = new List<Item>();
+            //get childs
+            do
+            {
+                List<Item> column = items.Where(i => i.ItemId == parent).ToList();
+                nodes.AddRange(column);
+                if (column.Count > 0)
                 {
-                    parent = tmp.ItemId;
-                    List<Item> childs = items.Where(i => i.ItemId == parent).ToList();
-                    if (childs.Count > 0)
-                    {
-                        ChossedList cl = new ChossedList(childs, childs.IndexOf(tmp));
-                        res.Table.Add(cl);
-                    }
-                    tmp = parent;
-                } while (parent != null);
-                res.Table.Reverse();
-                parent = current;
-                do
-                {
-                    List<Item> childs = items.Where(i => i.ItemId == parent).ToList();
-                    if (childs.Count > 0)
-                    {
-                        ChossedList cl = new ChossedList(childs, 0);
-                        res.Table.Add(cl);
-                    }
-                    else
-                        break;
-                    parent = childs[0];
-                } while (true);
+                    ChossedList cl = new ChossedList(column, 0);
+                    res.Table.Add(cl);
+                }
+                else
+                    break;
+                parent = column[0];
+            } while (true);
+            //get notes
+            res.Notes = notes.Where(n => nodes.Contains(n.ItemId)).ToList();
+            if (res.Notes.Count > 0)
+            {
+                res.Title = res.Notes[0].Title;
+                res.Text = res.Notes[0].Content;
             }
             else
             {
-
+                res.Title = "no title";
+                res.Text = "no content";
             }
-
 
             return res;
         }
 
+        public ViewModel MakeVmNoteId(List<Note> notes, List<Item> items, int noteId)
+        {
+            var note = notes.Where(x => x.Id == noteId).FirstOrDefault();
+            if (note == null)
+                return MakeVM(notes, items);
+            var parent = items.Where(x => x.Id == note.ItemId.Id).FirstOrDefault();
+            if (parent == null)
+                return MakeVM(notes, items);
+            var res = MakeVmForItem(notes, items, parent);
+
+            return res;
+        }
 
         public override string ToString()
         {
