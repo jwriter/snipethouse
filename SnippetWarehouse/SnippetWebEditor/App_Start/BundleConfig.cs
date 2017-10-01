410cb5e1f65723fc4c970777b60ed88980354c01
Maxim <makbykov@yandex.ru>
2017-10-02 00:01:57 +0300

    работаем с Views

--- SnippetWarehouse/SnippetWebEditor/App_Start/BundleConfig.cs
+++ SnippetWarehouse/SnippetWebEditor/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace SnippetWebEditor
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/table.css"));
         }
     }
 }
