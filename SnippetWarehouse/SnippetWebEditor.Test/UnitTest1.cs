31391946c4e52b73345169b4a0bf24f5327ccff9
Max <upornij@gmail.com>
2017-11-15 23:26:01 +0200

    Factory/ForView.cs

--- SnippetWarehouse/SnippetWebEditor.Test/UnitTest1.cs
+++ SnippetWarehouse/SnippetWebEditor.Test/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SnippetWebEditor.Models;
 using SnippetWebEditor.Controllers;
+using System.Linq;
 
 namespace SnippetWebEditor.Test
 {
@@ -16,6 +17,49 @@ namespace SnippetWebEditor.Test
             var notes = repo.GetNotes();
             var vm = new DataForView();
             var sel = vm.MakeData(notes, items);
+        }
+
+        [TestMethod]
+        public void TestTreeFindParent()
+        {
+            var tree = new Factory.Tree();
+            tree.Construct(null, null);
+
+            int min = 2;
+            int max = 13;
+            var items = Enumerable.Range(min, max)
+                .Select(i => new Item { Id = i })
+                .ToList();
+            tree.Construct(items, null);
+
+            for (int i = min; i <= max; i++)
+            {
+                Factory.Tree.Point o = tree.FindPoinById(i);
+                Assert.AreEqual(o.Content.Id, i);
+            }
+
+            for (int i = min + 1; i < max - 2; i++)
+            {
+                int removed = i;
+                Item link = items[removed];
+                items.Remove(link);
+                tree.Construct(items, null);
+                Factory.Tree.Point o2 = tree.FindPoinById(link.Id);
+                Assert.AreEqual(o2.Content.Title, "root");
+
+                for (int j = min; j <= max; j++)
+                {
+                    Factory.Tree.Point o = tree.FindPoinById(j);
+                    if (link.Id != j)
+                        Assert.AreEqual(o.Content.Id, j);
+                    else
+                        Assert.AreEqual(o.Content.Title, "root");
+                }
+
+                items.Insert(removed, link);
+            }
+            
+
         }
     }
 }
--- SnippetWarehouse/SnippetWebEditor/Factory/ForView.cs
+++ SnippetWarehouse/SnippetWebEditor/Factory/ForView.cs
@@ -1,6 +1,7 @@
 ﻿using SnippetWebEditor.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -21,80 +22,100 @@ namespace SnippetWebEditor.Factory
 
         public static DataForView MakeModelThroughNoteId(Tree tree, int noteId)
         {
-            return null;
+            DataForView res = new DataForView();
+            res.Table = tree.ExrtractTableByItem(noteId);
+            var last = GetLastSelectedNode(res.Table);
+            if (last != null)
+            {
+                if (last.Type == NodeType.Folder)
+                    res.IsContent = false;
+                else
+                {
+                    Item maybeNote = tree.GetNoteById(noteId).Content;
+                    if (maybeNote is Note)
+                    {
+                        res.IsContent = true;
+                        res.Content = ((Note)maybeNote).Content;
+                        res.TitleContent = maybeNote.Title;
+                    }
+                    else
+                    {
+                        res.IsContent = false;
+                    }
+                }
+            }
+            return res;
         }
 
         public static DataForView MakeModelThroughItemId(Tree tree, int itemId)
         {
-            return null;
+            DataForView res = new DataForView();
+            res.Table = tree.ExrtractTableByItem(itemId);
+            return res;
+        }
+
+        private static Node GetLastSelectedNode(List<List<Node>> table)
+        {
+            List<Node> list = table[table.Count - 1];
+            var res = list.Where(n => n.IsSelected).FirstOrDefault();
+            return res;
         }
 
         public static DataForView MakeModelThroughRoot(Tree tree)
         {
-            return null;
+            DataForView res = new DataForView();
+            res.Table = tree.ExrtractTableByItem(Tree.RootId);
+            return res;
         }
     }
 
     public class Tree
     {
+        public static readonly int RootId = -42;
+        public static readonly string RootTitle = "root";
+
         private Point _root = null;
         private List<Point> _items;
         private List<Point> _notes;
         
-        //TODO: linq
         public void Construct(List<Item> Items, List<Note> Notes)
         {
-            _root = new Point();
-            _items = new List<Point>(Items.Count);
-            for (int i = 0; i < Items.Count; i++)
+            _root = new Point() { Content = new Item { Id = RootId, ItemId = null, Title = RootTitle } };
+            _items = Items?.Select(i => new Point { Content = i }).ToList() ?? (new List<Point>());
+            _notes = Notes?.Select(n => new Point { Content = n }).ToList() ?? (new List<Point>());
+
+            foreach (var i in _items)
             {
-                _items[i].Content = Items[i];
-            }
-            _notes = new List<Point>(Notes.Count);
-            for (int i = 0; i < Notes.Count; i++)
-            {
-                _notes[i].Content = Notes[i];
-            }
-            
-            for (int i = 0; i < Items.Count; i++)
-            {
-                var tmp = FindParentItem(_items[i]);
-                tmp.Children.Add(_items[i]);
-                _items[i].Parent = tmp;
+                var tmp = FindParentItem(i);
+                tmp.Children.Add(i);
+                i.Parent = tmp;
             }
 
-            for (int i = 0; i < Items.Count; i++)
+            foreach (var n in _notes)
             {
-                var tmp = FindParentItem(_items[i]);
-                tmp.Children.Add(_items[i]);
-                _items[i].Parent = tmp;
-            }
-            for (int i = 0; i < Notes.Count; i++)
-            {
-                var tmp = FindParentItem(_notes[i]);
-                tmp.Children.Add(_notes[i]);
-                _notes[i].Parent = tmp;
-            }
+                var tmp = FindParentItem(n);
+                tmp.Children.Add(n);
+                n.Parent = tmp;
+            }            
         }
-
-        //TODO: test 
+        
         private Point FindParentItem(Point point)
         {
-            Item wanted = point.Content.ItemId;
-            if (wanted == null) return _root;
-            int parentId = wanted.Id;
+            Item parent = point.Content.ItemId;
+            if (parent == null) return _root;
+            int parentId = parent.Id;
             return FindPoinById(parentId);
         }
 
-        private Point FindPoinById(int parentId)
+        public Point FindPoinById(int parentId)
         {
             int min = 0;
             int max = _items.Count;
             do
             {
                 int nowIndex = min + ((max - min) / 2);
-                Point res = _items[max / 2];
-
+                Point res = _items[nowIndex];
+                
                 int nowId = res.Content.Id;
                 if (parentId == nowId) return res;
                 if (parentId > nowId)
@@ -102,31 +123,61 @@ namespace SnippetWebEditor.Factory
                 else
                     max = nowIndex;
 
-                if (min == max)
+                if (max - min <= 1)
                 {
+                    if (_items[min].Content.Id == parentId)
+                        return _items[min];
+                    if (_items[max].Content.Id == parentId)
+                        return _items[max];
                     return _root;
                 }
-
             } while (true);
         }
 
-        public object FindItem(int itemId)
+        public List<List<Node>> ExrtractTableByItem(int itemId)
         {
             Point itemContainer = FindPoinById(itemId);
-            List<List<Point>> list = new List<List<Point>>();
+            List<List<Node>> res = new List<List<Node>>();
             Point parent = itemContainer.Parent;
+            Point current = itemContainer;
             while (parent != _root)
             {
-                list.Insert(0, parent.Children);
+                res.Insert(0, parent.Children.Select(
+                    ch => new Node()
+                    {
+                        Caption = ch.Content.Title,
+                        index = (ch.Content is Item) ? "i" + ch.Content.Id : "n" + ch.Content.Id,
+                        IsSelected = ch.Equals(current),
+                        Type = (ch.Content is Item) ? NodeType.Folder : NodeType.Article
+                    }).ToList());
+                current = parent;
                 parent = parent.Parent;
-            } 
+            }
 
+            current = itemContainer;
+            while (current.Children.Count > 0)
+            {
+                res.Add(current.Children.Select(
+                    ch => new Node()
+                    {
+                        Caption = ch.Content.Title,
+                        index = (ch.Content is Item) ? "i" + ch.Content.Id : "n" + ch.Content.Id,
+                        IsSelected = ch.Equals(current.Children[0]),
+                        Type = (ch.Content is Item) ? NodeType.Folder : NodeType.Article
+                    }).ToList());
+                current = current.Children[0];
+            }
 
             return null;
         }
 
+        public Point GetNoteById(int noteId)
+        {
+            return _notes.Where(n => n.Content.Id == noteId).FirstOrDefault();
+        }
+        
         //элемент дерева
-        class Point
+        public class Point
         {
             public Point Parent;
             public List<Point> Children = new List<Point>(4);
@@ -140,19 +191,19 @@ namespace SnippetWebEditor.Factory
     /// </summary>
     public class DataForView
     {
-        public string MainTitle { get; }
-        public List<List<Node>> Table { get; }
-        public bool IsContent { get; }
-        public string TitleContent { get; }
-        public string Content { get; }
+        public string MainTitle { get; set; }
+        public List<List<Node>> Table { get; set; }
+        public bool IsContent { get; set; }
+        public string TitleContent { get; set; }
+        public string Content { get; set; }
     }
 
     public class Node
     {
-        public bool IsSelected { get; }
-        public NodeType Type { get; }
-        public string Caption { get; }
-        public string index { get; }
+        public bool IsSelected { get; set; }
+        public NodeType Type { get; set; }
+        public string Caption { get; set; }
+        public string index { get; set; }
     }
 
     public enum NodeType
